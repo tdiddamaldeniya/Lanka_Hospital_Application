85d4ba39dfd80488d6996fc0f1c14fb5617fd7f4
tdiddamaldeniya <85049757+tdiddamaldeniya@users.noreply.github.com>
2023-02-01 09:55:47 +0530

    Delete Test_Function_01.test.js

--- Test_Function_01.test.js
+++
@@ -1,5 +0,0 @@
-const Patient_Function = require('./Test_Function_01');
-
-test(' System will display all patinet  count in hospital. in this case 100 patient in ward and 4 wards in hospital ', () => {
-  expect(Patient_Function(4, 100)).toBe(400);
-});
